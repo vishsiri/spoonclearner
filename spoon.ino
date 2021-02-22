e00b41ae064ca491d059930a568c6a853f709540
vishsiri <s.vish555@gmail.com>
2021-02-22 21:17:40 +0700

    Update spoon.ino

--- spoon.ino
+++ spoon.ino
@@ -1,12 +1,35 @@
-void setup(){
+#include <LowPower.h>
 
-define bnt1 1
-define bnt2 2
-define bnt3 3
-
-
+void setup() {
+  pinMode(1,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  attachInterrupt(0,fuc1,RISING);
+  attachInterrupt(1,fuc2,RISING);
+  attachInterrupt(2,fuc3,RISING);
 }
 
 void loop() {
+  if 
+}
 
+void fuc1()               //btn1
+{
+  digitalWrite(6,HIGH);
+  delay(30000);           
+  digitalWrite(6,LOW);;
+}
+
+void fuc2()                //btn2
+{
+  digitalWrite(7,HIGH);
+  delay(20000);           
+  digitalWrite(7,LOW);
+}
+
+void fuc3()               //btn3
+{
+  digitalWrite(8,HIGH);
+  delay(10000);           
+  digitalWrite(8,LOW);
 }
